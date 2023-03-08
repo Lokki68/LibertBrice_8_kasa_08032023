54f75951ddad65bf45a137b6f54458f92f9c9f4e
Brice <bricel67@gmail.com>
2023-03-08 13:50:13 +0100

    init 1st step

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.scss'
+function App() {
+  return (
+    <h1 className={styles.appTest} >
+     Hello app
+      <span> Je suis le span</span>
+    </h1>
+  );
+}
+
+export default App;
