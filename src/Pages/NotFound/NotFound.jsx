1dac808c326e199bb555e5dcc9282eb92c146757
Brice <bricel67@gmail.com>
2023-03-08 17:25:17 +0100

    NotFound & ajout de variable sass

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import Styles from './NotFound.module.scss'
+import React from "react";
+import Styles from "./NotFound.module.scss";
 
 const NotFound = () => {
   return (
-    <div>
-      NotFound
+    <div className={Styles.notFound}>
+      <div className={Styles.content}>
+        <h2>404</h2>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+      </div>
+      <a href="/">Retourner sur la page d'accueil</a>
     </div>
   );
 };
