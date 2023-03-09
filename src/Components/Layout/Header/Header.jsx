18b6a700d541992aba6c679f17fc3f635e07b18e
Brice <bricel67@gmail.com>
2023-03-09 11:16:08 +0100

    Modif navLink Nav Active

--- src/Components/Layout/Header/Header.jsx
+++ src/Components/Layout/Header/Header.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import logo from "../../../Assets/logo.png";
 import Styles from "./Header.module.scss";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const { pathname } = useLocation();
+
   return (
     <div className={Styles.header}>
       <img className={Styles.headerLogo} src={logo} alt="Kasa Logo" />
       <nav>
-        <a href="/">Accueil</a>
-        <a href="/about">A Propos</a>
+        <NavLink
+          to="/"
+          className={pathname === "/" ? Styles.navItemActive : Styles.navItem}
+        >
+          Accueil
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={
+            pathname === "/about" ? Styles.navItemActive : Styles.navItem
+          }
+        >
+          A Propos
+        </NavLink>
       </nav>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.scss";
 // import App from './Components/App';
 
 import Layout from "./Components/Layout/Layout";
@@ -10,18 +10,18 @@ import Logement from "./Pages/Logement/Logement";
 import About from "./Pages/About/About";
 import NotFound from "./Pages/NotFound/NotFound";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Layout>
-      <BrowserRouter>
+    <BrowserRouter>
+      <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/logement/:id" element={<Logement />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </Layout>
+      </Layout>
+    </BrowserRouter>
   </React.StrictMode>
 );
